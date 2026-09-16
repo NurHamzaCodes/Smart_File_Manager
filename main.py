26d8647a186d78d732a48b59d1f9208453c1eae9
Nur Hamza <nurhamza888@gmail.com>
2026-09-16 07:30:56 +0600

    Complete

--- main.py
+++ main.py
@@ -3,13 +3,22 @@ from tkinter import filedialog
 import tkinter as tk
 import shutil
 
-root = tk.Tk()
-root.withdraw()
+def opening():
+    print(25*"="+" File Manger System "+25*"=")
 
-folder = filedialog.askdirectory()
+def select_folder():
 
-if not folder:
-    raise SystemExit
+    root = tk.Tk()
+    root.withdraw()
+
+    folder = filedialog.askdirectory()
+
+    if not folder:
+        raise SystemExit
+
+    print("You select: ",folder)
+
+    return folder
 
 category = {
     ".mp4": "Videos",
@@ -33,11 +42,45 @@ category = {
     ".jpeg": "Images",
     ".gif": "Images",
     ".bmp": "Images",
+    ".o":"O Files",
+    ".exe": "Executable Files"
 }
 
-for file in Path(folder).iterdir():
-    if file.is_file():
-        file_category = category.get(file.suffix.lower(), "Others")
-        cut = Path(folder) / file_category
-        cut.mkdir(parents=True, exist_ok=True)
-        shutil.move(str(file), str(cut / file.name))
+def work(folder):
+    print("\nWORKING....")
+    count = 0
+    for file in Path(folder).iterdir():
+        if file.is_file():
+            key = file.suffix.lower()
+            if key in category:
+                cut = Path(folder) / category[key]
+                cut.mkdir(parents=True,exist_ok=True)
+                shutil.move(str(file),str(cut/file.name))
+                print(file.name+"-->"+category[key])
+                count+=1
+
+
+            else:
+                cut = Path(folder) / "Others"
+                cut.mkdir(parents=True,exist_ok=True)
+                shutil.move(str(file),str(cut/file.name))
+                print(file.name+"--> Others")
+                count+=1
+
+    return count
+
+def ending(folder,count):
+    print()
+    print(25*"="+" Summarize The Task ",25*"=")
+    for fol in Path(folder).iterdir():
+        if fol.is_dir():
+            print(fol.name+"-->",len(list(fol.iterdir())),"Files")
+
+    print("\nTotal Transferred Files: ",count)
+    print()
+
+if __name__ == "__main__":
+    opening()
+    folder = select_folder()
+    count = work(folder)
+    ending(folder,count)
